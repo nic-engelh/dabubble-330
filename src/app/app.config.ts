63c4f8e6179885806d979090383ea0ed3e32f1b5
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-08-22 19:47:30 +0200

    added firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,34 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'dabubble-330',
+        appId: '1:800106272913:web:c0cb27877ba554e460c730',
+        databaseURL:
+          'https://dabubble-330-default-rtdb.europe-west1.firebasedatabase.app',
+        storageBucket: 'dabubble-330.appspot.com',
+        apiKey: 'AIzaSyCV2rfEpdiod6Loa7F0Ns-QizvPz9z35-4',
+        authDomain: 'dabubble-330.firebaseapp.com',
+        messagingSenderId: '800106272913',
+        measurementId: 'G-W5D72JRRJB',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage()),
+  ],
 };
