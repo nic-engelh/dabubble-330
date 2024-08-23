31bbe08422d38a274740b8afda2db0f527d5db2e
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-08-23 18:02:11 +0200

    Added testing functions to DataService: snapshot collection retrival

--- src/app/components/test-messenger/test-messenger.component.ts
+++ src/app/components/test-messenger/test-messenger.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { DataService } from '../../services/data-service/data.service';
 import { CommonModule } from '@angular/common';
+import { User } from '../../../models/user.class';
+import { Observable, Subscriber, Subscription } from 'rxjs';
+import { OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-test-messenger',
@@ -9,27 +12,43 @@ import { CommonModule } from '@angular/common';
   templateUrl: './test-messenger.component.html',
   styleUrl: './test-messenger.component.scss'
 })
-export class TestMessengerComponent {
-
+export class TestMessengerComponent implements OnInit, OnDestroy {
+  user = new User();
+  userSub: Subscription = new Subscription();
   data: any;
 
-  constructor(private dataService: DataService) {}
-
-  addData() {
-    this.dataService
-      .setDocument('testCollection', 'testDoc', { name: 'John Doe', age: 30 })
-      .subscribe({
-        next: () => console.log('Document successfully written!'),
-        error: (err) => console.error('Error writing document: ', err),
-      });
+  constructor(private dataService: DataService) {
+    this.user.username = 'Craven';
   }
 
-  getData() {
-    this.dataService
-      .getDocument('testCollection', 'testDoc')
-      .subscribe((docData) => {
-        this.data = docData;
-      });
+  async saveData() {
+    const data = this.user.toJson();
+    await this.dataService.setDocument('users', `${this.user.id}`, data);
   }
+
+  async getData() {
+    try {
+      const data = await this.dataService.getDocument('users', `${this.user.id}`);
+      console.log(data);
+      this.data = data
+    } catch (error) {
+      console.error('Error retrieving document:', error);
+    }
+  }
+
+  ngOnInit(): void {
+    this.userSub.add(this.dataService.getCollectionRealTime('users')
+    .subscribe({
+      next: (userData) => console.log(userData),
+      error: (error) => console.error(error),
+      complete: () => console.log('complete')
+    }));
+
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
+
 
 }
--- src/app/services/data-service/data.service.ts
+++ src/app/services/data-service/data.service.ts
@@ -1,25 +1,60 @@
 import { Injectable } from '@angular/core';
-import { Firestore, doc, setDoc, getDoc } from '@angular/fire/firestore';
-import { DocumentData } from 'firebase/firestore';
+import {
+  Firestore,
+  doc,
+  setDoc,
+  getDoc,
+  onSnapshot,
+} from '@angular/fire/firestore';
+import { DocumentData, collection, getFirestore } from 'firebase/firestore';
 import { Observable, from } from 'rxjs';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
-
-  constructor(private database: Firestore) { }
+  constructor(private database: Firestore) {}
 
   // Method to set a document in Firestore
-  setDocument(collectionPath: string, docId: string, data: any): Observable<void> {
-    const docRef = doc(this.database, `${collectionPath}/${docId}`);
-    return from(setDoc(docRef, data));
+  async setDocument(
+    collectionName: string,
+    documentId: string,
+    data: any
+  ): Promise<void> {
+    const documentRef = doc(this.database, collectionName, documentId);
+    await setDoc(documentRef, data);
   }
 
-   // Method to get a document from Firestore
-   getDocument(collectionPath: string, docId: string): Observable<DocumentData | undefined> {
-    const docRef = doc(this.database, `${collectionPath}/${docId}`);
-    return from(getDoc(docRef).then((snapshot) => snapshot.data()));
+  // Method to get a document from Firestore
+  async getDocument(collectionName: string, documentId: string): Promise<any> {
+    const documentRef = doc(this.database, collectionName, documentId);
+    const documentSnapshot = await getDoc(documentRef);
+    if (documentSnapshot.exists()) {
+      return documentSnapshot.data();
+    } else {
+      throw new Error('Document does not exist');
+    }
   }
+
+  getCollectionRealTime(
+    collectionName: string
+  ): Observable<any> {
+    return new Observable((Subscriber) => {
+      const db = getFirestore();
+      const collectionRef = collection(db, collectionName);
+      onSnapshot(
+        collectionRef,
+        (querySnap) => {
+          const data = querySnap.docs.map((docSnap) => ({
+            id: docSnap.id,
+            ...docSnap.data(),
+          }));
+          Subscriber.next(data);
+        },
+        (error) => Subscriber.error(error)
+      );
+    });
+  }
+
+  
 }
