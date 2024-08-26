ee22b6c974c4b4df2cc84046f816abfa90ad25fd
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-08-26 19:20:55 +0200

    added example threadId and imported needed Classes

--- src/app/components/message-input/message-input.component.ts
+++ src/app/components/message-input/message-input.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Message } from '../../../models/message.class';
+import { Conversation } from '../../../models/conversation.class';
+import { User } from '../../../models/user.class';
 
 @Component({
   selector: 'app-message-input',
@@ -8,5 +11,12 @@ import { Component } from '@angular/core';
   styleUrl: './message-input.component.scss'
 })
 export class MessageInputComponent {
+
+  threadId = "30040944-9e8d-4d01-a84b-a03c70ea58c7";
+  // collection "threads"
+  // subcollection: "messages"
+  // path threads/30040944-9e8d-4d01-a84b-a03c70ea58c7/messages
+
+
 
 }
