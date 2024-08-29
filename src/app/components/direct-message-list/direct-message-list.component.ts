e482e00ebaa9a1dda63c65106bab311fd5f1241d
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-08-29 15:18:37 +0200

    added img files to assets and swichted folder for fonts; Added basic html and logic to direct-message-list component

--- src/app/components/direct-message-list/direct-message-list.component.ts
+++ src/app/components/direct-message-list/direct-message-list.component.ts
@@ -1,4 +1,8 @@
+import { Conversation } from './../../../models/conversation.class';
+import { ConversationListService } from './../../services/conversation-list-service/conversation-list.service';
 import { Component } from '@angular/core';
+import { User } from '../../../models/user.class';
+
 
 @Component({
   selector: 'app-direct-message-list',
@@ -8,5 +12,19 @@ import { Component } from '@angular/core';
   styleUrl: './direct-message-list.component.scss'
 })
 export class DirectMessageListComponent {
+
+  // cache array for all direct messages participants/user
+  directMessageList: Conversation[] =  [];
+  testThread = new Conversation();
+  testUser = new User();
+
+
+  constructor (private chatListService: ConversationListService) {
+    this.testUser.username = "Clark Kent"
+    this.testThread.participants.push(this.testUser);
+    this.directMessageList.push(this.testThread);
+  }
+
+  getDirectMessageChats() {}
 
 }
