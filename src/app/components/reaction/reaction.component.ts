2953ab2e7620ce0f5efc59a14e55fb0d2f8c23dc
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-08-29 14:14:53 +0200

    Added more services and components for structure

---
+++ src/app/components/reaction/reaction.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reaction',
+  standalone: true,
+  imports: [],
+  templateUrl: './reaction.component.html',
+  styleUrl: './reaction.component.scss'
+})
+export class ReactionComponent {
+
+}
