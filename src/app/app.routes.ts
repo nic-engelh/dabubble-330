4cb409c39a676a0e8541afcb2a4274241d9f8db5
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-08-31 12:52:56 +0200

    Added comments and routerlink to chat-element for possible component routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { DirectMessageComponent } from './components/direct-message/direct-message.component';
+import { DirectMessageListComponent } from './components/direct-message-list/direct-message-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'chat/:id',
+    component: DirectMessageComponent
+  }
+
+];
