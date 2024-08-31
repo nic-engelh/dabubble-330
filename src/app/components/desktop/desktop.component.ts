3157d20518a56164103e25dae02de6408d80bbcd
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-08-31 20:20:04 +0200

    added desktop basic html/css and main-menu basic html/css

--- src/app/components/desktop/desktop.component.ts
+++ src/app/components/desktop/desktop.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MainChatComponent } from '../main-chat/main-chat.component';
+import { MainMenuComponent } from '../main-menu/main-menu.component';
+
 
 @Component({
   selector: 'app-desktop',
   standalone: true,
-  imports: [],
+  imports: [MainMenuComponent],
   templateUrl: './desktop.component.html',
   styleUrl: './desktop.component.scss'
 })
