ceb78fe6a22716f274b7335e616f6f3587b4a360
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-08-31 20:20:04 +0200

    added desktop basic html/css and main-menu basic html/css

--- src/app/components/main-menu/main-menu.component.ts
+++ src/app/components/main-menu/main-menu.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { DirectMessageListComponent } from '../direct-message-list/direct-message-list.component';
+
 
 @Component({
   selector: 'app-main-menu',
   standalone: true,
-  imports: [],
+  imports: [DirectMessageListComponent],
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.scss'
 })
