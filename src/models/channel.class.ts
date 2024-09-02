edd90f59b290e43907421e7aa855d4502682f716
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-09-02 11:14:50 +0200

    Added new component: channel list for menu; Added logic and services as well as html

--- src/models/channel.class.ts
+++ src/models/channel.class.ts
@@ -1,2 +1,39 @@
+import { User } from './user.class';
+import { Message } from './message.class';
+import { Conversation } from './conversation.class';
+import { v4 as uuidv4 } from 'uuid';
+
+
 export class Channel {
-}
+
+    constructor(
+      public id: string = "",
+      public name: string = "",
+      public description: string = "",
+      public member: User[] = [],
+      public messages: Message[] = [],
+      public conversations: Conversation[] = [],
+      public createdBy: User[] = [],
+      public createdAt: Date = new Date(),
+      public updatedAt: Date = new Date(),
+
+    ) {
+      this.id = uuidv4();
+    }
+
+    toJson(): any {
+      return {
+        id: this.id,
+        name: this.name,
+        description: this.description,
+        member: this.member.map(m => m.toJson()),
+        messages: this.messages.map(m => m.toJson()),
+        createdAt: this.createdAt.toISOString(),
+        updatedAt: this.updatedAt.toISOString(),
+      };
+    }
+  }
+
+
+
+
