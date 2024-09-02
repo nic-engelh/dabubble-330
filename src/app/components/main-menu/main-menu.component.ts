e0b976f5e9190a516f72ffcfba248c3154b2392a
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-09-02 12:03:07 +0200

    added css for channel-list

--- src/app/components/main-menu/main-menu.component.ts
+++ src/app/components/main-menu/main-menu.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { DirectMessageListComponent } from '../direct-message-list/direct-message-list.component';
+import { ChannelListComponent } from '../channel-list/channel-list.component';
 
 
 @Component({
   selector: 'app-main-menu',
   standalone: true,
-  imports: [DirectMessageListComponent],
+  imports: [DirectMessageListComponent, ChannelListComponent],
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.scss'
 })
