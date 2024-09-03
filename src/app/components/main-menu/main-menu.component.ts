d9419764193cf6efd0580d63b5aa32c7db5fe85d
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-09-03 16:07:55 +0200

    Added search bar for main-menu with basic logic

--- src/app/components/main-menu/main-menu.component.ts
+++ src/app/components/main-menu/main-menu.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { DirectMessageListComponent } from '../direct-message-list/direct-message-list.component';
 import { ChannelListComponent } from '../channel-list/channel-list.component';
+import { SearchComponent } from '../search/search.component';
 
 
 @Component({
   selector: 'app-main-menu',
   standalone: true,
-  imports: [DirectMessageListComponent, ChannelListComponent],
+  imports: [DirectMessageListComponent, ChannelListComponent, SearchComponent],
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.scss'
 })
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.scss'
+  styleUrl: './search.component.scss',
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  searchForm: FormGroup = new FormGroup({
+    search: new FormControl('', Validators.required),
+  });
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.searchForm = new FormGroup({
+      search: new FormControl('', Validators.required),
+    });
+  }
+
+  onSubmit(): void {
+    if (this.searchForm.valid) {
+      const searchControl = this.searchForm.get('search');
+      if (searchControl) {
+        const searchValue = searchControl.value;
+        console.log('Search value:', searchValue);
+        // Call API or perform search action here
+      }
+    }
+  }
 }
