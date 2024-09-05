2f3c6950ccf836069023e543df9e3da84b6e597e
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-09-05 13:35:05 +0200

    added new components for user auth

---
+++ src/app/components/user-authentication/avatar-selection/avatar-selection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-avatar-selection',
+  standalone: true,
+  imports: [],
+  templateUrl: './avatar-selection.component.html',
+  styleUrl: './avatar-selection.component.scss'
+})
+export class AvatarSelectionComponent {
+
+}
