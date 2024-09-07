ccc539391938b1142842ecdc8896ad4b8754d465
Matthias146 <126074233+Matthias146@users.noreply.github.com>
2024-09-07 17:16:46 +0200

    added form validation

--- src/app/components/message-input/message-input.component.ts
+++ src/app/components/message-input/message-input.component.ts
@@ -4,19 +4,28 @@ import { Component, OnInit, SimpleChanges } from '@angular/core';
 import { Message } from '../../../models/message.class';
 import { Conversation } from '../../../models/conversation.class';
 import { MessageService } from '../../services/message-service/message.service';
-import { FormControl } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-message-input',
   standalone: true,
-  imports: [FormsModule, RouterModule],
+  imports: [ReactiveFormsModule, RouterModule, CommonModule, FormsModule],
   templateUrl: './message-input.component.html',
   styleUrl: './message-input.component.scss',
 })
 export class MessageInputComponent implements OnInit {
   threadId = '30040944-9e8d-4d01-a84b-a03c70ea58c7'; // Will be given
+  chatForm: FormGroup;
+  messages: string[] = [];
   // collection "threads"
   // subcollection: "messages"
   // path threads/30040944-9e8d-4d01-a84b-a03c70ea58c7/messages
@@ -27,8 +36,13 @@ export class MessageInputComponent implements OnInit {
   // ACTIVE USER === SENDER
   constructor(
     private messageService: MessageService,
-    private messagingService: MessagingService
-  ) {}
+    private messagingService: MessagingService,
+    private fb: FormBuilder
+  ) {
+    this.chatForm = this.fb.group({
+      message: ['', [Validators.required, Validators.minLength(1)]],
+    });
+  }
 
   ngOnInit(): void {}
 
@@ -42,14 +56,41 @@ export class MessageInputComponent implements OnInit {
     // input === string;
   }
 
-  sendMessage() {
-    const newMessage = this.messageService.createMessage(
-      this.threadId,
-      this.formInputValues,
-      this.user
-    );
+  // sendMessage() {
+  //   if (this.chatForm.valid) {
+  //     const newMessage = this.messageService.createMessage(
+  //       this.threadId,
+  //       this.formInputValues,
+  //       this.user
+  //     );
 
-    this.messagingService.setMessagetoConversation(this.threadId, newMessage);
+  //     this.messagingService.setMessagetoConversation(this.threadId, newMessage);
+  //   }
+  // }
+
+  sendMessage() {
+    if (this.chatForm.valid) {
+      // Formularwert auslesen
+      const messageContent = this.chatForm.get('message')?.value;
+
+      // Erstelle die Nachricht mit dem MessageService
+      const newMessage = this.messageService.createMessage(
+        this.threadId, // ID des Gesprächs
+        messageContent, // Der eigentliche Nachrichteninhalt (als string)
+        this.user // Aktueller Benutzer, der die Nachricht sendet
+      );
+
+      // Nachricht zur Konversation hinzufügen
+      this.messagingService
+        .setMessagetoConversation(this.threadId, newMessage)
+        .then(() => {
+          console.log('Nachricht erfolgreich gesendet!');
+          this.chatForm.reset(); // Formular nach erfolgreichem Absenden zurücksetzen
+        })
+        .catch((error) => {
+          console.error('Fehler beim Senden der Nachricht:', error);
+        });
+    }
   }
 
   //function getUserSender(){}
