26d578739b76488c3c4062bc2a0b5074a48583f3
Niklas <110847147+nic-engelh@users.noreply.github.com>
2025-01-25 18:16:17 +0100

    Added basic html and subscriptions for data-services

--- src/app/components/channel-main-chat/channel-main-chat.component.ts
+++ src/app/components/channel-main-chat/channel-main-chat.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication-service/authentication.service';
+import { ChannelService } from '../../services/channel-service/channel.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-channel-main-chat',
@@ -7,6 +11,53 @@ import { Component } from '@angular/core';
   templateUrl: './channel-main-chat.component.html',
   styleUrl: './channel-main-chat.component.scss'
 })
-export class ChannelMainChatComponent {
+export class ChannelMainChatComponent implements OnInit, OnDestroy {
+  channelThreads: any;
+  currentUser: any;
+  currentChannelId: any;
+  private channelSubscription!: Subscription;
+
+
+  /*
+  TODO
+  on opening:
+  -get active channel (channel-service)
+  -read channelThreads into variables
+  -read first threads-conversationMessages and corresponding sender for each channelThread
+  -render channelthreads in hmtl (logged user to right, member to the left)
+  -implement emoji function and array in the message/thread class
+
+  on using:
+  -sending a new channelThread-Message, creates a new thread within the channel AND in collection "threads"
+  -each message is saved collectivley within message-collection (at to comp: message-input or message-service)
+  -updates messages in each collection --> messages-service
+  -clicking on "antworten" opens corresponding conversation/thread as modal or so.
+  -clicking on message opens up options as a modal above it
+  */
+
+
+  constructor(
+    private channelService: ChannelService,
+    private authService: AuthenticationService) { }
+
+
+  ngOnInit(): void {
+    this.authService.getCurrentUser().subscribe((user) => {
+      this.currentUser = user;
+    });
+    this.channelSubscription = this.channelService.channelId$.subscribe((channelId) => {
+      this.currentChannelId = channelId;
+    })
+  }
+
+
+
+
+  ngOnDestroy() {
+    if (this.channelSubscription) {
+      this.channelSubscription.unsubscribe();
+    }
+  }
+
 
 }
