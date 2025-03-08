420964e43519d5c9c5da6a4774f6f4f479b36858
Niklas <110847147+nic-engelh@users.noreply.github.com>
2025-03-08 09:53:12 +0100

    Changed conversation model

--- src/models/conversation.class.ts
+++ src/models/conversation.class.ts
@@ -4,24 +4,32 @@ import { v4 as uuidv4 } from 'uuid';
 
 // A conversation is a selection of messages within a chat of two members. A thread is a type of conversation within a channel.
 export class Conversation {
-  constructor(
-    public id: string = "",
-    public name: string = "",
-    public participants: User[] = [],
-    public creator: User = new User,
-    public createdAt: Date = new Date(),
-    public updatedAt: Date = new Date(),
-    public firstMessage?: Message,
-    public numberOfMessages: number = 0,
-  ) {
-    this.id = uuidv4();
+
+  id: string;
+  name: string;
+  participants: User[];
+  creator: User;
+  createdAt: Date;
+  updatedAt: Date;
+  firstMessage?: Message;
+  numberOfMessages: number;
+
+
+  constructor() {
+    this.id = uuidv4(),
+      this.name = '',
+      this.participants = [],
+      this.creator = new User(),
+      this.createdAt = new Date(),
+      this.updatedAt = new Date(),
+      this.numberOfMessages = 0
   }
 
   toJson(): any {
     return {
       id: this.id,
       name: this.name,
-      participants: this.participants.map(p => p?.toJson() || null),
+      participants: this.participants.map(p => p.toJson()),
       creator: this.creator?.toJson() || null,
       createdAt: this.createdAt.toISOString(),
       updatedAt: this.updatedAt.toISOString(),
